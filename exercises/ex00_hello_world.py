809bd4e9d2f497858ecb4d232c658a4bba5b612d
Sara Eckel <sara28@ad.unc.edu>
2022-08-17 23:53:15 -0400

    Finished Exercise 00!

--- exercises/ex00_hello_world.py
+++ exercises/ex00_hello_world.py
@@ -1,5 +1,5 @@
 """My first program for COMP110."""
 __author__ = "730470511"
 print("Hello, world.")
-print(Hello, world how are you)
-print(Hello, world what is your favorite meal?)
+print("Hello, world. how are you")
+print("Hello, world. what is your favorite meal?")
